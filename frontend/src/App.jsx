a0a09ed78099f748ba141a4d0d4a94c1c6276391
Harshpreetkaur98 <harshpreet.149kaur@gmail.com>
2025-04-05 19:07:34 +0100

    deleted extra files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import ScanCvPage from "./components/ScanCV/ScanCvPage";
 import Login from "./components/Login/Login";
-// import AdminLogin from "./components/Portals/AdminPortal/APortalPage/APortalPage";
-// import CandidateLogin from "./components/Portals/CandidatePortal/CPortalPage/CPortalPage";
 import EmployerApp from "./components/Portals/EmployerPortal/EmployerApp";
 import CandidateApp from "./components/Portals/CandidatePortal/CandidateApp";
 import SignUp from "./components/SignUp/SignUp";
@@ -19,7 +17,6 @@ function App() {
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Employer-Portal/*" element={<EmployerApp />} />
         <Route path="/Candidate-Portal/*" element={<CandidateApp />} />
-        {/* <Route path="/Candidate-Portal" element={<CandidateLogin />} /> */}
       </Routes>
       <Footer />
     </Router>
