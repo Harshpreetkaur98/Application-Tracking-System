52dea70807000dafceed92a78445b54f766a0cae
Harshpreetkaur98 <harshpreet.149kaur@gmail.com>
2025-04-23 23:58:45 +0100

    Quote

--- frontend/src/components/Quote/Quote.jsx
+++ frontend/src/components/Quote/Quote.jsx
@@ -1,11 +1,13 @@
 import "react";
 import "./quote.css";
 import QuoteImage from "../../assets/images/QuoteImage.png";
+import bridingImage from "../../assets/images/bridigingGap.png";
 
 const Quote = () => {
   return (
     <div className="quote-container">
       <img src={QuoteImage} alt="Quote" className="quote-image" />
+      <img src={bridingImage} alt="Quote" className="quote-image" />
     </div>
   );
 };
